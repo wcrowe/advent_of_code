036790e52db3f8d85d1ee9bd7f3e50e789651627
wcrowe <crowe.will@gmail.com>
2022-12-31 12:53:03 -0500

    added struct

--- day1/src/main.rs
+++ day1/src/main.rs
@@ -3,12 +3,44 @@
 
 use tokio::{fs::File, io::AsyncReadExt};
 
+// struct Elf
+#[derive(Debug)]
+struct Elf {
+    name: String,
+    calories: i32,
+} 
+struct Elves {
+    elves: Vec<Elf>,
+}
+impl Elves {
+    fn new() -> Elves {
+        Elves { elves: Vec::new() }
+    }
+    fn add(&mut self, elf: Elf) {
+        self.elves.push(elf);
+    }
+    fn get(&self, index: usize) -> &Elf {
+        &self.elves[index]
+    }
+    fn len(&self) -> usize {
+        self.elves.len()
+    }
+}
+    
+}
+
+
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
     let mut file = File::open("src/data/input.txt").await?;
     let mut contents = String::new();
     file.read_to_string(&mut contents).await?;
     let mut lines = contents.lines();
-    println!("File contents: {}", lines.next().unwrap());
+    // loop over lines
+    for line in lines {
+        println!("Line: {}", line.trim());
+
+    }
+
     Ok(())
 }
