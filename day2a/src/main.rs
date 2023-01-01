67dd78a30f56f30ee48026ef664f16192bff26cb
wcrowe <crowe.will@gmail.com>
2023-01-01 16:17:23 -0500

    day 2 starting

---
+++ day2a/src/main.rs
@@ -0,0 +1,23 @@
+enum opponent{
+    A,
+    B,
+    C,
+}
+enum mine{
+    X,
+    Y,
+    Z,
+}
+enum opponent {
+    Rock,
+    Paper,
+    Scissors,
+}
+enum result {
+    Win,
+    Lose,
+    Draw,
+}
+fn main() {
+    println!("Hello, world!");
+}
