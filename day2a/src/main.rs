52a59ca9b2f931eaa2246d2daf6a2c4facf14c26
wcrowe <crowe.will@gmail.com>
2023-01-01 21:54:59 -0500

    almost done

--- day2a/src/main.rs
+++ day2a/src/main.rs
@@ -22,27 +22,60 @@ fn calculate(a: usize, b: usize) -> usize {
     } else if a == Rock && b == Scissors {
         score = Win  + b;
     } else if a == Rock && b == Paper {
-        score = Lose + b;
+        score = Win + b;
     } else if a == Paper && b == Rock {
         score = Win + b;
     } else if a == Paper && b == Scissors {
         score = Lose + b;
     } else if a == Scissors && b == Paper {
-        score = Win + b;
+        score = Lose + b;
     } else if a == Scissors && b == Rock {
         score = Lose+ b;
     }
     score
+}
+fn letter_to_number(letter: &str) -> usize {
+    let mut result = 0;
+    if letter == "A" || letter == "X"{
+        result = Rock;
+    } else if letter == "B" || letter == "Y"{
+        result = Paper;
+    } else if letter == "C" || letter == "Z"{
+        result = Scissors;
+    }
+    result
+
+}
+
+fn convert_to_string(score: usize) -> String {
+    let mut result = String::new();
+    if score == 6 {
+        result = "Win".to_string();
+    } else if score == 0 {
+        result = "Lose".to_string();
+    } else if score == 3 {
+        result = "Draw".to_string();
+    }
+    result
 }
 
 fn main() {
     let draw1 = "A,Y";
     let val1 =draw1.split(",").nth(0).unwrap().trim();
     let val2 =draw1.split(",").nth(1).unwrap().trim();
-   let num1 = i32.parse(val1).unwrap();
-   let num2 = i32.parse(val2).unwrap();
-   //d let score = calculate(val1, val2);
-    println!("{} vs {}" , val1, val2.trim());
+   let num1 = letter_to_number(val1);
+   let num2 = letter_to_number(val2);
+    let score = calculate(num1, num2);
+    println!("score = {}", score);
+    // println!("{} vs {}" , val1, val2.trim());
+    // println!("{} vs {}" , num1, num2);
+    // println!("{} vs {}" , convert_to_string(num1), convert_to_string(num2));
+    // println!("{} vs {}" , convert_to_string(score), score);
+    // let draw2 = "B,Z";
+    // let val1 =draw2.split(",").nth(0).unwrap().trim();
+    // let val2 =draw2.split(",").nth(1).unwrap().trim();
+
+    // println!("{} vs {}" , val1, val2.trim());
 
 
 }
